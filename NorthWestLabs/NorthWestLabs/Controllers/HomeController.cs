af25f6881d169dbeb558015b9573fd1bd18a6553
curnowbr <curnowbr@gmail.com>
2018-12-05 15:49:49 -0700

    about / contact

--- NorthWestLabs/NorthWestLabs/Controllers/HomeController.cs
+++ NorthWestLabs/NorthWestLabs/Controllers/HomeController.cs
@@ -13,5 +13,15 @@ namespace NorthWestLabs.Controllers
         {
             return View();
         }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
     }
 }
