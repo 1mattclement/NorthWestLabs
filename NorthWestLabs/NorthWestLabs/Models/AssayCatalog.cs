aa27072556328b11dcc2a2391668d55b4ac170d5
curnowbr <curnowbr@gmail.com>
2018-12-05 14:56:29 -0700

    Another Model

---
+++ NorthWestLabs/NorthWestLabs/Models/AssayCatalog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NorthWestLabs.Models
+{
+    [Table("AssayCatalog")]
+    public class AssayCatalog
+    {
+        [Key]
+        public int AssayTypeID { get; set; }
+        public string ProtocolDescription { get; set; }
+        public string LitReference { get; set; }
+        public double EstCompletionTime { get; set; }
+        public string AssayName { get; set; }
+
+    }
+}
