8ddafa120389ca9b0d7871800d09f573b4a379a0
curnowbr <curnowbr@gmail.com>
2018-12-05 14:40:51 -0700

    Another Model

---
+++ NorthWestLabs/NorthWestLabs/Models/WorkOrders.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NorthWestLabs.Models
+{
+    [Table("WorkOrders")]
+    public class WorkOrders
+    {
+        [Key]
+        public int WorkOrderID { get; set; }
+
+        public string Comments { get; set; }
+
+        [DisplayName("Rush Order")]
+        public bool RushOrder { get; set; }
+
+        [DisplayName("Customer ID")]
+        [Required(ErrorMessage = "Please enter your customer ID")]
+        public int CustomerID { get; set; } //add virtual
+
+        [DisplayName("Payment Info")]
+        [Required(ErrorMessage = "Please enter your payment ID")]
+        public int PmtInfoID { get; set; }
+
+        [DisplayName("LTNumber")]
+        [Required(ErrorMessage = "Please enter your customer LTNumber")]
+        public int LTNumber { get; set; }
+
+  
+    }
+}
