7dc044bd6a25ba1630d79d5e355fe714d23aa193
1mattclement <1mattclement@gmail.com>
2018-12-05 18:38:46 -0700

    sokbnvoer

--- NorthWestLabs/NorthWestLabs/Models/Customers.cs
+++ NorthWestLabs/NorthWestLabs/Models/Customers.cs
@@ -22,26 +22,29 @@ namespace NorthWestLabs.Models
         [Required(ErrorMessage = "Please enter your last name!")]
         public string CustLastName { get; set; }
 
+        [DisplayName("Customer Address")]
         [Required(ErrorMessage = "Please enter your address!")]
         public string CustAddress { get; set; }
 
+        [DisplayName("Customer State")]
         [Required(ErrorMessage = "Please enter your city!")]
         public string CustState { get; set; }
 
-        [StringLength(5)]
+        [DisplayName("Customer Zip")]
         [Required(ErrorMessage = "Please enter your zipcode")]
-        public int CustZip { get; set; }
+        public int CustZIP { get; set; }
 
-
+        [DisplayName("Customer Phone")]
         [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
         [Required(ErrorMessage = "Please enter your phone number!")]
         public string CustPhone { get; set; }
 
+        [DisplayName("Customer Email")]
         [EmailAddress]
         [Required(ErrorMessage = "Please enter your email address!")]
         public string CustEmail { get; set; }
 
-        public int? SalesRepID { get; set; } 
+        public int? EmployeeID { get; set; } 
         public virtual Employees Employees {get; set;}
        
         public string Comments { get; set; }
