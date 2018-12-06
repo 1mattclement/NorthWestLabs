8ee12ed3d380c4373dfd7f04696abc734cc96d4d
curnowbr <curnowbr@gmail.com>
2018-12-05 17:03:14 -0700

    working auth

--- NorthWestLabs/NorthWestLabs/Controllers/HomeController.cs
+++ NorthWestLabs/NorthWestLabs/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace NorthWestLabs.Controllers
 {
@@ -24,6 +25,11 @@ namespace NorthWestLabs.Controllers
             return View();
         }
 
+        public ActionResult Login()
+        {
+            return View();
+        }
+
         [HttpPost]
         public ActionResult Login(FormCollection form, bool rememberMe = false)
         {
@@ -32,10 +38,12 @@ namespace NorthWestLabs.Controllers
 
             if(string.Equals(username, "employee") && (string.Equals(password, "employee")))
             {
+                FormsAuthentication.SetAuthCookie(username, rememberMe);
                 return RedirectToAction("EmployeeInfo, Home"); //fix
             }
             else if (string.Equals(username, "customer") && (string.Equals(password, "customer")))
             {
+                FormsAuthentication.SetAuthCookie(username, rememberMe);
                 return RedirectToAction("CustomerInfo, Home"); //fix
             }
             else
